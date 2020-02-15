03966f6b1f8790a058fd3df4b72424e1aeab5415
PiotrGabrys <piotrek.gabrys@gmail.com>
2020-02-15 11:27:18 +0100

    lambdas init

--- mtdlearn/src.py
+++ mtdlearn/src.py
@@ -3,6 +3,8 @@ import numpy as np
 
 class MTD:
 
-    def __init__(self, n_dimensions, order):
+    def __init__(self, n_dimensions, order, lambdas_init='flat'):
         self.n_dimensions = n_dimensions
         self.order = order
+        if lambdas_init == 'flat':
+            self.lambdas_ = np.ones(order) / order
--- tests/tests.py
+++ tests/tests.py
@@ -1,1 +1,10 @@
 from mtdlearn import MTD
+import pytest
+import numpy as np
+
+
+def test_create_lambdas():
+    mtd = MTD(5, 4, lambdas_init='flat')
+    assert len(mtd.lambdas_) == 4
+    assert max(mtd.lambdas_) == min(mtd.lambdas_)
+    assert mtd.lambdas_[0] == 0.25
