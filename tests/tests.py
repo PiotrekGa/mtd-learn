1a31ee7a758bf53b044d4d2071c1652cfff95ef4
PiotrkGa <piotrek.gabrys@gmail.com>
2020-04-03 18:30:04 +0200

    test modified to new dataset

--- tests/tests.py
+++ tests/tests.py
@@ -1,22 +1,15 @@
 from mtdlearn.mtd import MTD
-from mtdlearn.preprocessing import parse_markov_matrix
+from mtdlearn.preprocessing import parse_markov_matrix, PathEncoder
+from mtdlearn.datasets import second_order
 import pytest
 import numpy as np
 import logging
 
 logger = logging.getLogger(__name__)
 
-x = np.array([[0, 0],
-              [1, 1],
-              [2, 2],
-              [0, 1],
-              [2, 1],
-              [2, 0],
-              [0, 1],
-              [2, 1],
-              [1, 1],
-              [1, 0]])
-y = np.array([0, 0, 2, 1, 1, 2, 0, 1, 2, 1])
+x = second_order['x']
+y = second_order['y']
+sample_weight = second_order['sample_weight']
 
 
 def test_create_indexes():
@@ -28,8 +21,11 @@ def test_ex_max():
 
     for seed in range(100):
         np.random.seed(seed)
+        pe = PathEncoder(2)
+        pe.fit(x, y)
+        x_tr, y_tr = pe.transform(x, y)
         mtd = MTD(3, 2, verbose=0, number_of_initiations=1)
-        mtd.fit(x, y)
+        mtd.fit(x_tr, y_tr)
         assert mtd.lambdas.shape == (2,)
         assert np.isclose(sum(mtd.lambdas), 1.0)
         assert max(mtd.lambdas) <= 1
@@ -41,8 +37,11 @@ def test_ex_max():
 
 
 def test_create_markov():
+    pe = PathEncoder(2)
+    pe.fit(x, y)
+    x_tr, y_tr = pe.transform(x, y)
     mtd = MTD(3, 2, verbose=0)
-    mtd.fit(x, y)
+    mtd.fit(x_tr, y_tr)
     assert mtd.transition_matrix.max() <= 1.0
     assert mtd.transition_matrix.min() >= 0.0
     assert np.isclose(mtd.transition_matrix.sum(1).max(), 1.0)
@@ -62,5 +61,6 @@ def test_parsing_input_type():
 
 def test_parsing_output_type():
 
-    out = parse_markov_matrix(x)
-    assert out.shape == (20,)
+    x_ones = np.ones((81, 3))
+    out = parse_markov_matrix(x_ones)
+    assert out.shape == (243,)
