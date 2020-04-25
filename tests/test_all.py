05269ec98f4baaf5058aa0e8657d164a617769f3
PiotrGabrys <piotrek.gabrys@gmail.com>
2020-04-25 16:29:29 +0200

    tests modified

--- tests/test_all.py
+++ tests/test_all.py
@@ -208,39 +208,29 @@ def test_create_markov():
     assert mtd.transition_matrix.shape == (9, 3)
 
 
-# def test_predict():
-#     transition_matrices = data_for_tests['transition_matrices'].copy()
-#     lambdas = data_for_tests['lambdas'].copy()
-#     pe = PathEncoder(2)
-#     pe.fit(x, y)
-#     x_tr, y_tr = pe.transform(x, y)
-#     mtd = MTD(2,
-#               max_iter=0,
-#               verbose=0,
-#               number_of_initiations=1,
-#               lambdas_init=lambdas,
-#               transition_matrices_init=transition_matrices)
-#     mtd.fit(x_tr, y_tr)
-#     assert np.array_equal(mtd.predict(np.array([[0, 0], [0, 1], [1, 0], [1, 1]])), np.array([1, 1, 1, 0]))
+def test_predict():
+    transition_matrices = np.array([[[0.2, 0.8], [0.7, 0.3]], [[0.1, 0.9], [0.5, 0.5]]])
+    lambdas = np.array([0.5, 0.5])
+
+    x_tr = np.random.randint(2, size=(10, 2))
+    y_tr = np.random.randint(2, size=10)
+    mtd = MTD(2, max_iter=0, verbose=0, lambdas_init=lambdas, transition_matrices_init=transition_matrices)
+    mtd.fit(x_tr, y_tr)
+    assert np.array_equal(mtd.predict(np.array([[0, 0], [0, 1], [1, 0], [1, 1]])), np.array([1, 1, 1, 0]))
 
 
-# def test_predict_proba():
-#     transition_matrices = data_for_tests['transition_matrices'].copy()
-#     lambdas = data_for_tests['lambdas'].copy()
-#     pe = PathEncoder(2)
-#     pe.fit(x, y)
-#     x_tr, y_tr = pe.transform(x, y)
-#     mtd = MTD(2,
-#               max_iter=0,
-#               verbose=0,
-#               number_of_initiations=1,
-#               lambdas_init=lambdas,
-#               transition_matrices_init=transition_matrices)
-#     mtd.fit(x_tr, y_tr)
-#     assert np.isclose(mtd.predict_proba(np.array([[0, 0], [0, 1], [1, 0], [1, 1]])), np.array([[0.22, 0.78],
-#                                                                                                [0.46, 0.54],
-#                                                                                                [0.34, 0.66],
-#                                                                                                [0.58, 0.42]])).min()
+def test_predict_proba():
+    transition_matrices = np.array([[[0.2, 0.8], [0.7, 0.3]], [[0.1, 0.9], [0.5, 0.5]]])
+    lambdas = np.array([0.5, 0.5])
+
+    x_tr = np.random.randint(2, size=(10, 2))
+    y_tr = np.random.randint(2, size=10)
+    mtd = MTD(2, max_iter=0, verbose=0, lambdas_init=lambdas, transition_matrices_init=transition_matrices)
+    mtd.fit(x_tr, y_tr)
+    assert np.isclose(mtd.predict_proba(np.array([[0, 0], [0, 1], [1, 0], [1, 1]])), np.array([[0.15, 0.85],
+                                                                                               [0.35, 0.65],
+                                                                                               [0.4, 0.6],
+                                                                                               [0.6, 0.4]])).min()
 
 
 def test_mc_fit():
